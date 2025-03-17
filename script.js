71ac96370b4395a13fa2c071fed535a32b577e8e
mahfuz-7148 <mrahman7148@gmail.com>
2025-03-17 13:34:59 +0600

    done

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const login = () => {
-  const nameInput = document.getElementById("nameInput").value
-  const passInput = document.getElementById("passInput").value 
-  
+  const nameInput = document.getElementById("nameInput").value;
+  const passInput = document.getElementById("passInput").value;
 
-  if (!nameInput ) {
+  if (!nameInput && !passInput) {
     Swal.fire({
       icon: "error",
-      title: "The name is incorrect!",
+      title: "Please enter your name and password!",
+    });
+    return
+  }
+  else if (!nameInput) {
+    Swal.fire({
+      icon: "error",
+      title: "Please enter your name!",
+    });
+  
+  } else if (passInput !== "123456") {
+    Swal.fire({
+      icon: "error",
+      title: "The Password is incorrect!",
+      text: "Please contact admin to get your login code.",
     });
   }
-  else if (passInput !== '123456') {
-     Swal.fire({
-       icon: "error",
-       title: "The Password is incorrect!",
-       text: "Please contact admin to get your login code.",
-     });
-  }
+   
+  
   else {
-    document.getElementById("nav").classList.remove('hidden')
+    document.getElementById("nav").classList.remove("hidden");
     document.getElementById("home").classList.add("hidden");
     document.getElementById("main").classList.remove("hidden");
     Swal.fire({
@@ -26,26 +34,24 @@ const login = () => {
       draggable: true,
     });
   }
-
-}
+};
 
 const logOut = () => {
   document.getElementById("nav").classList.add("hidden");
   document.getElementById("home").classList.remove("hidden");
   document.getElementById("main").classList.add("hidden");
-}
-
-
+};
 
 const showLoader = () => {
-  document.getElementById("loader").classList.remove('hidden')
-}
+  document.getElementById("loader").classList.remove("hidden");
+};
 const hideLoader = () => {
-  document.getElementById("loader").classList.add('hidden')
-}
+  document.getElementById("loader").classList.add("hidden");
+};
 
+
+ 
 const createLessonButtons = async () => {
-
   const response = await fetch(
     "https://openapi.programming-hero.com/api/levels/all"
   );
@@ -59,7 +65,7 @@ const createLessonButtons = async () => {
     const button = document.createElement("button");
 
     button.innerHTML = `
-          <button onclick='fetchWordsForLevel(${lesson.level_no})' class="btn border border-[#422AD5] text-[#422AD5] rounded-md font-semibold text-sm mx-2">  
+          <button id='btn-${lesson.level_no}' onclick='fetchWordsForLevel(${lesson.level_no})' class="btn border border-[#422AD5] text-[#422AD5] rounded-md font-semibold text-sm mx-2">  
           <span><i class="ri-book-open-fill text-base"></i></span> ${lesson.lessonName}
             </button>
           `;
@@ -68,72 +74,35 @@ const createLessonButtons = async () => {
   });
 };
 
-const showDetailsModal = async (videoId) => {
-  const url = await fetch(
-    `https://openapi.programming-hero.com/api/word/${videoId}`
-  );
-  const response = await url.json();
-  const data = response.data;
-  displayWordMOdal(data);
-};
-
-const displayWordMOdal = (detailsModal) => {
- 
-  document.getElementById("wordDetailsModal").showModal();
-  const wordDetailsContent = document.getElementById("wordDetailsContent");
-  wordDetailsContent.innerHTML = `
-
-  
-
-     <div class='text-left'>
-     <h1 class="text-4xl font-semibold text-gray-800">${
-       detailsModal.word
-     } (<span><i class="ri-mic-ai-fill"></i> : </span>${
-    detailsModal.pronunciation
-  })</span></h1>   
-            
-            <h2 class="text-gray-900 font-semibold text-2xl">Meaning</h2>
-            <p class="text-gray-900 font-medium text-2xl">${
-              detailsModal.meaning
-            }</p>
-            <h2 class="text-gray-600 font-semibold">Example</h2>
-            <p class="text-gray-800">${detailsModal.sentence} </p>
-            <h2 class="text-gray-600 font-semibold">সমার্থক শব্দ</h2>
-     </div>
-   
-           <div class='space-x-5 text-left'>
-            ${detailsModal.synonyms
-              .map((mod) => {
-                return `<button class="bg-gray-100 text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200">${mod}</button>`;
-              })
-              .join("")}</div>
-   
-         <div class="modal-action justify-start">
-      <form method="dialog">
-       <button class=" bg-purple-600 text-white px-4 py-2 rounded-lg hover:bg-purple-700 cursor-pointer">
-                Complete Learning
-            </button>
-      </form>
-    </div>
-    
-  `;
-};
-
 const fetchWordsForLevel = async (level) => {
-    showLoader();
+  showLoader();
 
   const response = await fetch(
     `https://openapi.programming-hero.com/api/level/${level}`
   );
   const data = await response.json();
+
   const words = data.data;
+
+   const activeClassRemove = () => {
+     const activeBtns = document.querySelectorAll(".active");
+     activeBtns.forEach((activeBtn) => {
+       activeBtn.classList.remove("active");
+     });
+  };
+  
+   activeClassRemove();
+   const clickedButton = document.getElementById(`btn-${level}`);
+   clickedButton.classList.add("active");
 
   const wordList = document.getElementById("words");
   wordList.innerHTML = "";
 
   if (words.length === 0) {
     document.getElementById("notFound").classList.add("hidden");
-    wordList.innerHTML = `
+
+    setTimeout(() => {
+      wordList.innerHTML = `
 
         <div class="p-20 rounded-lg col-span-3">
 
@@ -151,17 +120,20 @@ const fetchWordsForLevel = async (level) => {
         </div>
   
     `;
+    }, 1000);
 
+    setTimeout(() => {
+      hideLoader();
+    }, 1000);
 
-   setTimeout(() => {
-    hideLoader()
-   }, 1000);
-    
     return;
   }
-  
+ 
+
+ 
+
   words.forEach((word) => {
-    console.log(word.word)
+    console.log(word.word);
     const div = document.createElement("div");
 
     div.innerHTML = `
@@ -169,35 +141,112 @@ const fetchWordsForLevel = async (level) => {
   <div class="card-body space-y-2">
     <h2 class="font-bold text-3xl">${word.word}</h2>
     <h2 class="font-medium text-lg">Meaning / Pronounciation</h2>
-    <h2 class="font-semibold text-3xl mb-10">${word.meaning} / ${word.pronunciation}</h2>
+    <h2 class="font-semibold text-3xl mb-10">${
+      word.meaning === null || word.meaning === undefined
+        ? "অর্থ পাওয়া যায়নি"
+        : word.meaning
+    } / ${word.pronunciation}</h2>
  
     <div class="card-actions justify-between p-5">
-    <button onclick='showDetailsModal(${word.id})' class="btn"><i class="ri-information-2-fill text-2xl"></i></button>
-      <button onclick="speaker('${word.word}')" class="btn"><i class="ri-volume-up-fill text-2xl"></i></button>
+    <button onclick='showDetailsModal(${
+      word.id
+    })' class="btn"><i class="ri-information-2-fill text-2xl"></i></button>
+      <button onclick="speaker('${
+        word.word
+      }')" class="btn"><i class="ri-volume-up-fill text-2xl"></i></button>
     </div>
   </div>
 </div>
     `;
+
     setTimeout(() => {
       wordList.appendChild(div);
-      hideLoader()
+      hideLoader();
     }, 1000);
-      document.getElementById("notFound").classList.add("hidden");
+    document.getElementById("notFound").classList.add("hidden");
   });
-   
 };
+
+const showDetailsModal = async (videoId) => {
+  const url = await fetch(
+    `https://openapi.programming-hero.com/api/word/${videoId}`
+  );
+  const response = await url.json();
+  const data = response.data;
+  
+  displayWordMOdal(data);
+};
+
+const displayWordMOdal = (detailsModal) => {
+  document.getElementById("wordDetailsModal").showModal();
+  const wordDetailsContent = document.getElementById("wordDetailsContent");
+  wordDetailsContent.innerHTML = `
+
+     <div class='text-left space-y-5'>
+     <h1 class="text-4xl font-semibold text-gray-800">${
+       detailsModal.word
+     } (<span><i class="ri-mic-ai-fill"></i> : </span>${
+    detailsModal.pronunciation
+  })</span></h1>   
+            
+            <h2 class="text-gray-900 font-semibold text-2xl">Meaning</h2>
+            <p class="text-gray-900 font-medium text-2xl">${
+              detailsModal.meaning === null ||
+              detailsModal.meaning === undefined
+                ? "অর্থ পাওয়া যায়নি"
+                : detailsModal.meaning
+            }</p>
+            <h2 class="text-gray-900 font-semibold text-2xl">Example</h2>
+            <p class="text-gray-800 text-xl font-medium">${
+              detailsModal.sentence
+            } </p>
+            <h2 class="text-gray-900 font-semibold text-xl mb-5">সমার্থক শব্দ গুলো</h2>
+     </div>
+   
+           <div class='space-x-5 text-left'>
+            ${detailsModal.synonyms
+              .map((modalButton) => {
+                return `<button class="bg-gray-100 text-gray-900 text-xl px-4 py-2 rounded-lg hover:bg-gray-200">${modalButton}</button>`;
+              })
+              .join("")}</div>
+   
+         <div class="modal-action justify-start">
+      <form method="dialog">
+       <button class=" btn rounded-md bg-[#422AD5] text-white hover:bg-[#2c1f80] cursor-pointer">
+                Complete Learning
+            </button>
+      </form>
+    </div>
+    
+  `;
+};
+
 
 function speaker(wordSpeak) {
-  console.log('click')
+  let voices = window.speechSynthesis.getVoices();
 
-  const utterance = new SpeechSynthesisUtterance(wordSpeak);
-  utterance.lang = "en-EN"; // English
-  window.speechSynthesis.speak(utterance);
-};
+  function speak() {
+    const utterance = new SpeechSynthesisUtterance(wordSpeak);
+    utterance.lang = "en-US";
 
+    const femaleVoice = voices.find((voice) => voice.name.includes("Zira"));
 
+    if (femaleVoice) {
+      utterance.voice = femaleVoice;
+    } else {
+      console.log("No female voice found, using default voice");
+    }
 
-
+    window.speechSynthesis.speak(utterance);
+  }
+  if (voices.length === 0) {
+    window.speechSynthesis.onvoiceschanged = () => {
+      voices = window.speechSynthesis.getVoices();
+      speak();
+    };
+  } else {
+    speak();
+  }
+}
 createLessonButtons();
-
 
